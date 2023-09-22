dd492cadea12bf0703cc06a0e87247827980f8ad
laguagu <49702497+laguagu@users.noreply.github.com>
2023-09-22 21:31:40 +0300

    Endpoint for deleting messages

--- backend/contollers/messageController.js
+++ backend/contollers/messageController.js
@@ -3,29 +3,38 @@
 import Message from "../models/Message.js";
 
 export const getMessages = async (req, res) => {
-    try {
-        const messages = await Message.find()
-        res.status(200).json(messages)
-    } catch (error) {
-        res.status(500).json({error: error.message})
-    }
-}
+  try {
+    const messages = await Message.find();
+    res.status(200).json(messages);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
 export const createMessage = async (req, res) => {
-    try{
-        const message = new Message(req.body);
-        await message.save();
-        res.status(201).json(message);
-    } catch(error) {
-        res.status(400).json({ error: error.message });
-    }
-}
+  try {
+    const message = new Message(req.body);
+    await message.save();
+    res.status(201).json(message);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 export const getMessageById = async (req, res) => {
-    try {
-        const message = await Message.findById(req.params.id)
-        res.status(200).json(message)
-    } catch (error) {
-        res.status(500).json({error: error.message})
-    }
-}
+  try {
+    const message = await Message.findById(req.params.id);
+    res.status(200).json(message);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const deleteMessages = async (req, res) => {
+  try {
+    await Message.deleteMany({});
+    res.status(200).json({message: "All messages deletet succesfully"})
+  } catch (error) {
+    res.status(500).json({ error: "An error occured during message deleting"})
+  }
+};
--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,12 +1,13 @@
 // Sisältää nimensä mukaan kaikki api reitit minne voidaan tehdä HTTP pyyntöjä.
 // Jokainen reitti on yhdistetty johonkin controller luokkaan
 import Express from "express";
-import { getMessages, createMessage, getMessageById } from "../contollers/messageController.js"
+import { getMessages, createMessage, getMessageById, deleteMessages } from "../contollers/messageController.js"
 
 const router = Express.Router()
 
 router.get("/", getMessages)
 router.post('/', createMessage);
 router.get("/:id", getMessageById)
+router.delete("/", deleteMessages)
 
 export default router;
