60898f2b6d3ab04f6159e793314a31578d6aeef8
laguagu <49702497+laguagu@users.noreply.github.com>
2023-09-22 11:18:17 +0300

    added end point for finding message  get by id

--- backend/contollers/messageController.js
+++ backend/contollers/messageController.js
@@ -20,3 +20,12 @@ export const createMessage = async (req, res) => {
         res.status(400).json({ error: error.message });
     }
 }
+
+export const getMessageById = async (req, res) => {
+    try {
+        const message = await Message.findById(req.params.id)
+        res.status(200).json(message)
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+}
--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,11 +1,12 @@
 // Sisältää nimensä mukaan kaikki api reitit minne voidaan tehdä HTTP pyyntöjä.
 // Jokainen reitti on yhdistetty johonkin controller luokkaan
 import Express from "express";
-import { getMessages, createMessage } from "../contollers/messageController.js"
+import { getMessages, createMessage, getMessageById } from "../contollers/messageController.js"
 
 const router = Express.Router()
 
 router.get("/", getMessages)
 router.post('/', createMessage);
+router.get("/:id", getMessageById)
 
 export default router;
