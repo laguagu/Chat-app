d825a1fdd311fe24e2e692994facf779c06000f9
laguagu <49702497+laguagu@users.noreply.github.com>
2023-10-06 17:17:26 +0300

    ChatApp copyright text update

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -25,7 +25,7 @@ function Copyright(props) {
     >
       {"Copyright © "}
       <Link color="inherit" href="https://mui.com/">
-        Your Website
+        ChatApp
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
